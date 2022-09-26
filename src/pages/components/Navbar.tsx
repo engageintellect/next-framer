cbf07a70684ce8212796975fe6efc8e9f1a61b48
jchadwick <jchadwick@firstam.com>
2022-09-25 20:40:37 -0700

    init framer motion example

---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { motion } from 'framer-motion';
+
+const Navbar = () => {
+	return (
+
+			<div className="bg-white sticky top-0 p-2">
+		<motion.nav 
+			initial={{ 
+				x: -500,
+				opacity: 0,
+				scale: 0.5,
+			 }}
+
+			animate={{
+				x: 0,
+				opacity: 1,
+				scale: 1,
+			}}
+
+			transition={{
+				duration: 1,
+				type: 'spring',
+				// stiffness: 100,
+			}}
+
+			 >
+
+
+				<div className="flex gap-4 max-w-7xl mx-auto text-xl">
+					<div>
+						<a href="/">
+							Home
+							</a>
+					</div>
+				<div>
+						<a href="#API">
+							API
+							</a>
+					</div>
+				<div>
+						<a href="mailto:jchadwick@firstam.com">
+							Contact
+							</a>
+					</div>
+
+				</div>
+
+		</motion.nav>
+			</div>
+	)
+}
+
+
+export default Navbar
